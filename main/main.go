a9b4fd36556b85ddaa2b3cf8a55a666d21e277f2
B3liv3r <mashanit@gmail.com>
2023-03-15 19:01:15 +0300

    add README.md

---
+++ main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	romanMap := map[byte]int{'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+	var firstNum, secondNum int
+	fmt.Print("Введите  ")
+}
+
+func romanSum() {
+}
